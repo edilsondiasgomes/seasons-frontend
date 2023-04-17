e51af2a28bf67227641c83308401cd6f97abd050
edilson <edilsondiasgomes@gmail.com>
2023-04-17 19:08:02 -0300

    add primeflex angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrimengModule } from '../primeng/primeng.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { PrimengModule } from './primeng/primeng.module';
 
 @NgModule({
   declarations: [
